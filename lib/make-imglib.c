c79765af77e76177899e457dac87c3620c206e4a
esa.peuha <esa.peuha>
2015-06-02 11:16:04 +0000

    * lib/make-imglib.c, lib/xwin.c: Change png_ptr->jmpbuf to 	  png_jmpbuf(png_ptr) 	* lib/xwin.c: Change voidp to png_voidp

--- lib/make-imglib.c
+++ lib/make-imglib.c
@@ -86,7 +86,7 @@ scan_image_directory ()
     png_ptr = png_create_read_struct (PNG_LIBPNG_VER_STRING, 0, 0, 0);
     info_ptr = png_create_info_struct (png_ptr);
 
-    if (setjmp (png_ptr->jmpbuf)) {
+    if (setjmp (png_jmpbuf (png_ptr))) {
       fclose (f);
       continue;
     }
